acc81d10e0334696810ae809de484b753b7a4796
Guite <info@guite.de>
2014-02-13 11:10:52 +0100

    readded compiled files

---
+++ de.guite.modulestudio.help.tests/xtend-gen/de/guite/modulestudio/help/tests/manual/BasicApplicationClassTestsSpec.java
@@ -0,0 +1,30 @@
+package de.guite.modulestudio.help.tests.manual;
+
+import org.jnario.lib.Assert;
+import org.jnario.lib.Should;
+import org.jnario.runner.ExampleGroupRunner;
+import org.jnario.runner.Named;
+import org.jnario.runner.Order;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * This class tests certain aspects of the Application meta class.
+ */
+@Named("Basic application class tests")
+@RunWith(ExampleGroupRunner.class)
+@SuppressWarnings("all")
+public class BasicApplicationClassTestsSpec {
+  /**
+   * First dummy test.
+   */
+  @Test
+  @Named("Just a dummy test")
+  @Order(1)
+  public void _justADummyTest() throws Exception {
+    boolean _should_be = Should.<Integer>should_be(Integer.valueOf((2 - 1)), Integer.valueOf(5));
+    Assert.assertFalse("\nExpected (2-1) should not be 5 but"
+     + "\n     2-1 is " + new org.hamcrest.StringDescription().appendValue(Integer.valueOf((2 - 1))).toString() + "\n", _should_be);
+    
+  }
+}
